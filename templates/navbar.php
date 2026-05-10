7069f412356ee5cce9208d7853bb1e2781d82a5f
arivernando21 <arivernando781@gmail.com>
2026-05-10 13:16:52 +0700

    UI navbar

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,28 +1,63 @@
+<?php
+
+$current_page =
+    basename($_SERVER['PHP_SELF']);
+
+?>
+
 <div class="navbar no-print">
 
     <?php if (isset($_SESSION['role'])) { ?>
 
         <?php if ($_SESSION['role'] == 'admin') { ?>
 
-            <a href="../../modules/admin/dashboard.php">
+            <a href="../../modules/admin/dashboard.php"
+
+            class="<?= $current_page == 'dashboard.php'
+            ? 'active'
+            : ''; ?>">
 
                 Dashboard
 
             </a>
 
-            <a href="../../modules/admin/patients.php">
+            <a href="../../modules/admin/patients.php"
+
+            class="<?= in_array(
+
+            $current_page,
+
+            [
+                'patients.php',
+                'add_patient.php',
+                'patient_history.php',
+                'visit_detail.php'
+            ]
+
+            )
+
+            ? 'active'
+            : ''; ?>">
 
                 Kelola Pasien
 
             </a>
 
-            <a href="../../modules/admin/activity_logs.php">
+            <a href="../../modules/admin/activity_logs.php"
+
+            class="<?= $current_page == 'activity_logs.php'
+            ? 'active'
+            : ''; ?>">
 
                 Activity Logs
 
             </a>
 
-            <a href="../../modules/admin/users.php">
+            <a href="../../modules/admin/users.php"
+
+            class="<?= $current_page == 'users.php'
+            ? 'active'
+            : ''; ?>">
 
                 Kelola User
 
@@ -32,7 +67,21 @@
 
         <?php if ($_SESSION['role'] == 'nurse') { ?>
 
-            <a href="../../modules/nurse/dashboard.php">
+            <a href="../../modules/nurse/dashboard.php"
+
+            class="<?= in_array(
+
+            $current_page,
+
+            [
+                'dashboard.php',
+                'assessment.php'
+            ]
+
+            )
+
+            ? 'active'
+            : ''; ?>">
 
                 Dashboard Nurse
 
@@ -42,7 +91,22 @@
 
         <?php if ($_SESSION['role'] == 'doctor') { ?>
 
-            <a href="../../modules/doctor/dashboard.php">
+            <a href="../../modules/doctor/dashboard.php"
+
+            class="<?= in_array(
+
+            $current_page,
+
+            [
+                'dashboard.php',
+                'assessment.php',
+                'lab_review.php'
+            ]
+
+            )
+
+            ? 'active'
+            : ''; ?>">
 
                 Dashboard Doctor
 
@@ -52,7 +116,11 @@
 
         <?php if ($_SESSION['role'] == 'inpatient') { ?>
 
-            <a href="../../modules/inpatient/dashboard.php">
+            <a href="../../modules/inpatient/dashboard.php"
+
+            class="<?= $current_page == 'dashboard.php'
+            ? 'active'
+            : ''; ?>">
 
                 Dashboard
 
